264d1af3cc515bf65880590dfc30968b2a4ef163
LGuitron <lguitron1995@gmail.com>
2016-11-16 21:57:22 -0600

    Funcion para seleccionar operacion especifica

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -17,45 +17,93 @@ def Menu():
         print("Selecciona una opcion: \n")
         print("1. Introduce nuevos conjuntos")
         print("2. Realiza operacion con conjuntos")
-        print("3. Salir")
+        print("3. Ver conjuntos A,B y U")
+        print("4. Salir")
         respuesta=input("Tu seleccion: ")
 
         if(respuesta=="1"):
-            
             U=CrearUniverso()
             A=CrearConjunto(U,"A")
             B=CrearConjunto(U,"B")
             print("\n")
             conjuntosEstablecidos=True
        
-        elif(respuesta=="2" and conjuntosEstablecidos==False):
-            print("Primero introduce nuevos conjuntos para poder realizar operaciones")
+        elif((respuesta=="2" or respuesta=="3") and conjuntosEstablecidos==False):
+            print("Debes introducir nuevos conjuntos para realizar esta seleccion")
             print("\n")
 
         elif(respuesta=="2"):
-            interseccion,soloA,soloB,union = Operaciones(A,B,U)
-            A_complemento = Complemento(A,U)
-            B_complemento = Complemento (B,U)
-            cPotencia = Potencia(U)
-            print("Los resultados son:\n")
-            ImprimirConjunto(interseccion,"A Interseccion B")
-            ImprimirConjunto(soloA,"A - B")
-            ImprimirConjunto(soloB,"B - A")
-            ImprimirConjunto(union,"A U B")
-            ImprimirConjunto(A_complemento,"A Complemento")
-            ImprimirConjunto(B_complemento,"B Complemento")
-            print ("Conjunto potencia Universo")
-            for combination in Potencia(U):
-                print(combination)
-            print("\n")
+            SeleccionarOperacion(A,B,U)
             
         elif(respuesta=="3"):
+            ImprimirConjunto(A,"A")
+            ImprimirConjunto(B,"B")
+            ImprimirConjunto(U,"U")
+            
+        elif(respuesta=="4"):
             terminar=True
 
         else:
             print("Opcion no valida, escoje un numero entre 1 y 3")
             print("\n")
 
+
+def SeleccionarOperacion(A,B,U):
+    
+    interseccion,soloA,soloB,union = Operaciones(A,B,U)
+    print("Selecciona la operacion que quieres realizar: \n")
+    print("1. A ∩ B")
+    print("2. A ∪ B")
+    print("3. A - B")
+    print("4. B - A")
+    print("5. Complemento A")
+    print("6. Complemento B")
+    print("7. Potencia A")
+    print("8. Potencia B")
+    print("9. Potencia U \n")
+    respuesta=input("Tu seleccion: ")
+
+    if(respuesta=="1"):
+        ImprimirConjunto(interseccion,"A ∩ B")
+        
+    elif(respuesta=="2"):
+        ImprimirConjunto(union,"A ∪ B")
+        
+    elif(respuesta=="3"):
+        ImprimirConjunto(soloA,"A - B")
+        
+    elif(respuesta=="4"):
+        ImprimirConjunto(soloB,"B - A")
+        
+    elif(respuesta=="5"):
+        A_complemento = Complemento(A,U)
+        ImprimirConjunto(A_complemento,"Complemento A")
+        
+    elif(respuesta=="6"):
+        B_complemento = Complemento(A,U)
+        ImprimirConjunto(B_complemento,"Complemento B")
+        
+    elif(respuesta=="7"):
+        print ("Conjunto potencia A")
+        for combination in Potencia(A):
+            print(combination)
+        print("\n")
+        
+    elif(respuesta=="8"):
+        print ("Conjunto potencia B")
+        for combination in Potencia(B):
+            print(combination)
+        print("\n")
+        
+    elif(respuesta=="9"):
+        print ("Conjunto potencia U")
+        for combination in Potencia(U):
+            print(combination)
+        print("\n")
+        
+    else:
+        print("Opcion no valida")
+        resultado=0
 
 ##Con esta funcion se crea el conjunto universo##
 def CrearUniverso():
@@ -90,7 +138,7 @@ def CrearConjunto(universo , nombre_conjunto_nuevo):
     return conjuntoNuevo
 
 ##Esta funcion devuelve 4 conjuntos##
-##(A-B), (B-A), (A^B), (A U B)
+##(A-B), (B-A), (A ∩ B), (A ∪ B)
 def Operaciones(cA,cB, universo):
     
     index=0
